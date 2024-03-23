be4eb557adcecfca2627eb197b918d41c00779e0
Yaphet <YaphetLee2002@qq.com>
2024-03-23 21:05:05 +0800

    inital commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'],
-    },
+    output: "standalone",
 };
 
 export default nextConfig;
--- src/app/gallery.tsx
+++ src/app/gallery.tsx
@@ -12,7 +12,7 @@ export function GalleryDemo() {
                 duration: 0.8,
                 ease: "easeInOut",
             }}
-            className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+            className="mt-8 bg-gradient-to-br from-slate-50 to-slate-50 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
         >
             Gallery
         </motion.h1>
